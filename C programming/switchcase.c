99bc281cfdac1b33b1ad9dd2c9270509d226289f
Angshuman Halder <ahdirtyboy@gmail.com>
2016-07-09 16:40:23 +0530

    added switch case

---
+++ C programming/switchcase.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+
+char *chartype;
+
+void findchartype(int i) {
+  switch (i) {
+    case 0:
+      chartype = "NULL";
+      break;
+    case 7:
+      chartype = "Bell";
+      break;
+    case 8:
+      chartype = "Backspace";
+      break;
+    case 9:
+      chartype = "Tav";
+      break;
+    case 10:
+      chartype = "Line Feed";
+      break;
+    case 13:
+      chartype = "Carriage Return";
+      break;
+    case 32:
+      chartype = "Space";
+      break;
+    case 48:
+    case 49:
+    case 50:
+    case 51:
+    case 52:
+    case 53:
+    case 54:
+    case 55:
+    case 56:
+    case 57:
+      chartype = "Numbers";
+      break;
+    default:
+      chartype = "Character";
+  }
+}
+
+void showascii() {
+  int i;
+  char c;
+  for(i = 0; i <= 127; i++) {
+    findchartype(i);
+    printf("%d = %c \t \t %s \n",i,i,chartype);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  showascii();
+  return 0;
+}
